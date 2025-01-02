a7906b9bb94862fabc633b0ba6a7d983dad1b363
Training Dept <training@sgsolutionsgroup.com>
2025-01-03 03:10:47 +0500

    branch added

---
+++ lib/branch/branch_one.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BranchOne extends StatefulWidget {
+  const BranchOne({super.key});
+
+  @override
+  State<BranchOne> createState() => _BranchOneState();
+}
+
+class _BranchOneState extends State<BranchOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('branc testing '),),
+    );
+  }
+}
