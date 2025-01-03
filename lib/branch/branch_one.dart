a37cca2682d613866d5120b8de9232ecb0238c79
Training Dept <training@sgsolutionsgroup.com>
2025-01-03 18:55:02 +0500

    title correction

--- lib/branch/branch_one.dart
+++ lib/branch/branch_one.dart
@@ -11,7 +11,7 @@ class _BranchOneState extends State<BranchOne> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('branc testing '),),
+      appBar: AppBar(title: Text('branch testing '),),
     );
   }
 }
